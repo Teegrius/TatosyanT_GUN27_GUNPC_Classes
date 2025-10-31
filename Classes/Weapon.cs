0f470f1cde8465866e6b7c8a91d740ea9b898f23
Teegrius <tatosian.tigr@yandex.ru>
2025-10-31 13:49:04 +0300

    Fixed the weapon.cs code

--- Classes/Weapon.cs
+++ Classes/Weapon.cs
@@ -1,4 +1,6 @@
-﻿namespace HomeWork
+﻿using System;
+
+namespace HomeWork
 {
     public class Weapon
     {
@@ -6,11 +8,43 @@
         public int MinDamage { get; }
         public int MaxDamage { get; }
 
+        private Random random;
+
         public Weapon(string name, int minDamage, int maxDamage)
         {
+            if (minDamage > maxDamage)
+            {
+                (minDamage, maxDamage) = (maxDamage, minDamage);
+                Console.WriteLine("Invalid input: min > max. Values have been swapped.");
+            }
+
+            if (minDamage < 0)
+            {
+                Console.WriteLine("Invalid input: min < 0. Set to 0.");
+                minDamage = 0;
+            }
+
+            if (maxDamage <= 0)
+            {
+                Console.WriteLine("Invalid input: max <= 0. Set to 10.");
+                maxDamage = 10;
+            }
+
             Name = name;
             MinDamage = minDamage;
             MaxDamage = maxDamage;
+
+            random = new Random();
+        }
+
+        public int GetDamage()
+        {
+            return random.Next(MinDamage, MaxDamage + 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Damage: {MinDamage}-{MaxDamage})";
         }
     }
 }
